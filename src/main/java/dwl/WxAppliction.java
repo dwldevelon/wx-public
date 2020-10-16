206ac1b25cabca9098ab4a92e7e0ab977c48b7b4
wenlong.ding <wenlong.ding@msxf.com>
2020-10-16 17:28:00 +0800

    init

---
+++ src/main/java/dwl/WxAppliction.java
@@ -0,0 +1,8 @@
+package dwl;
+
+/**
+ * @author wenlong.ding
+ * @date 2020/10/16 17:17
+ */
+public class WxAppliction {
+}
---
+++ src/main/java/dwl/controller/NotifyController.java
@@ -0,0 +1,8 @@
+package dwl.controller;
+
+/**
+ * @author wenlong.ding
+ * @date 2020/10/16 17:21
+ */
+public class IndexController {
+}
