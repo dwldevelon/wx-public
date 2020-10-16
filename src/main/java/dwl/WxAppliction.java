03769ec1800f27550f2be672d85e8bb2cb12831b
wenlong.ding <wenlong.ding@msxf.com>
2020-10-16 17:59:25 +0800

    init

--- src/main/java/dwl/WxAppliction.java
+++ src/main/java/dwl/WxAppliction.java
@@ -1,8 +1,15 @@
 package dwl;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author wenlong.ding
  * @date 2020/10/16 17:17
  */
+@SpringBootApplication
 public class WxAppliction {
+    public static void main(String[] args) {
+        SpringApplication.run(WxAppliction.class,args);
+    }
 }
--- src/main/java/dwl/controller/NotifyController.java
+++ src/main/java/dwl/controller/NotifyController.java
@@ -1,8 +1,17 @@
 package dwl.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author wenlong.ding
  * @date 2020/10/16 17:21
  */
-public class IndexController {
+@RestController
+public class NotifyController {
+
+    @GetMapping("/notify")
+    public Object wxNotify(){
+        return "success";
+    }
 }
