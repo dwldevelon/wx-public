ef2d014c969f39b3a8ae8359c2db1bf26c30e427
wenlong.ding <wenlong.ding-g@zkj.com>
2020-10-22 19:43:10 +0800

    init

--- src/main/java/dwl/controller/NotifyController.java
+++ src/main/java/dwl/controller/NotifyController.java
@@ -1,5 +1,6 @@
 package dwl.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/10/16 17:21
  */
 @RestController
+@Slf4j
 public class NotifyController {
 
     @GetMapping("/notify")
     public Object wxNotify(String echostr){
+        log.info("echostr:{}",echostr);
         return echostr;
     }
 }
