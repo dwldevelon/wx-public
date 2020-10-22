5807968b14b084d360d727a6f84aa8ff8dc44ff6
wenlong.ding <wenlong.ding-g@zkj.com>
2020-10-22 20:15:42 +0800

    init

--- src/main/java/dwl/controller/NotifyController.java
+++ src/main/java/dwl/controller/NotifyController.java
@@ -2,6 +2,8 @@ package dwl.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,8 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotifyController {
 
     @GetMapping("/notify")
-    public Object wxNotify(String echostr){
-        log.info("echostr:{}",echostr);
-        return echostr;
+    public Object wxNotify( @RequestBody String requestBody,
+                            @RequestParam("signature") String signature,
+                            @RequestParam("timestamp") String timestamp,
+                            @RequestParam("nonce") String nonce,
+                            @RequestParam("openid") String openid,
+                            @RequestParam(name = "encrypt_type", required = false) String encType,
+                            @RequestParam(name = "msg_signature", required = false) String msgSignature,
+                            @RequestParam String echostr){
+
+        log.info("\n接收微信请求：[openid=[{}], [signature=[{}], encType=[{}], msgSignature=[{}],"
+                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{},echostr=[{}]\n] ",
+                openid, signature, encType, msgSignature, timestamp, nonce, requestBody,echostr);
+
+        return "";
     }
 }
