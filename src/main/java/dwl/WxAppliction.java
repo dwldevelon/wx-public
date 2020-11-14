e55fb54c6e0440c1fc0bf93d3c5c09932906129e
wenlong.ding <wenlong.ding@zkjin.com>
2020-11-14 14:47:58 +0800

    11 14

--- src/main/java/dwl/WxAppliction.java
+++ src/main/java/dwl/WxAppliction.java
@@ -2,12 +2,14 @@ package dwl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author wenlong.ding
  * @date 2020/10/16 17:17
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class WxAppliction {
     public static void main(String[] args) {
         SpringApplication.run(WxAppliction.class,args);
