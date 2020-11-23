7a45734961a05e025c87b13abb3c84e92da5de0c
wenlong.ding <wenlong.ding@zkjin.com>
2020-11-23 14:47:25 +0800

    11 23

--- src/main/java/dwl/service/business/impl/HeadAndTailWrapperImpl.java
+++ src/main/java/dwl/service/business/impl/HeadAndTailWrapperImpl.java
@@ -5,6 +5,8 @@ import dwl.model.entity.ProcessTreeDto;
 import dwl.plugins.BeanRepository;
 import dwl.service.business.HeadAndTailWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,25 +17,30 @@ import java.util.stream.Collectors;
  * @date 2020/11/23 14:25
  */
 @Slf4j
+@Service
 public class HeadAndTailWrapperImpl extends BeanRepository implements HeadAndTailWrapper {
     @Override
-    public String wrapper(Integer code,String content) {
-        if(Objects.isNull(code)){
+    public String wrapper(Integer code, String content) {
+        if (Objects.isNull(code)) {
             return content;
         }
         ProcessTreeDto ptTree = processTreeService.findByCode(code);
-        if(Objects.isNull(ptTree)){
-            log.warn("wrapper失败，code对象不存在，code={}",code);
+        if (Objects.isNull(ptTree)) {
+            log.warn("wrapper失败，code对象不存在，code={}", code);
             return content;
         }
         List<ProcessTreeDto> ptTreeDtoList = processTreeService.findToRootByCode(code);
 
-        String result = ptTreeDtoList.stream().map(e->String.format("%s[%s]",e.getCode(),e.getName())).collect(Collectors.joining("/","/",""));
+        String result = ptTreeDtoList.stream().map(e -> String.format("%s[%s]", e.getCode(), e.getName())).collect(Collectors.joining("/", "/", ""));
 
         result += CommonConstant.RN;
-        result += content;
+        if (!StringUtils.isEmpty(content)) {
+            result += CommonConstant.RN;
+            result += content;
+            result += CommonConstant.RN;
+        }
         result += CommonConstant.RN;
         result += ptTree.getDescription();
-        return null;
+        return result;
     }
 }
